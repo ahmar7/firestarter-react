e97f40de9bde6da18a80952050ed46a0e73096a8
karloseq <87254977+ahmar7@users.noreply.github.com>
2023-04-09 23:57:45 +0500

    update

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Header from "../layout/Header/header";
-import TopBanner from "../components/TopBanner/TopBanner";
-import BelowBanner from "../components/BelowBanner/BelowBanner";
-import Features from "../components/Features/Features";
+import TopBanner from "../components/HomeComponents/TopBanner/TopBanner";
+import BelowBanner from "../components/HomeComponents/BelowBanner/BelowBanner";
+import Features from "../components/HomeComponents/Features/Features";
 import Advantages from "../components/HomeComponents/AdvantagesSection/Advantages";
-import Description from "../components/DescriptionSection/Description";
-import GetReady from "../components/GetReadySection/GetReady";
-import Tiers from "../components/TiersSection/Tiers";
-import TimeWrap from "../components/TimeWrap/TimeWrap";
-import Faq from "../components/Faq/Faq";
+import Description from "../components/HomeComponents/DescriptionSection/Description";
+import GetReady from "../components/HomeComponents/GetReadySection/GetReady";
+import Tiers from "../components/HomeComponents/TiersSection/Tiers";
+import TimeWrap from "../components/HomeComponents/TimeWrap/TimeWrap";
+import Faq from "../components/HomeComponents/Faq/Faq";
 import Footer from "../layout/Footer/Footer";
-import Carousel from "../components/UpComingProjects/Slider";
+import Carousel from "../components/HomeComponents/UpComingProjects/Slider";
 
 const Home = () => {
   return (
